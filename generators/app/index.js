04a263952a19123e81fb421c8ded60849b48dda6
Plou <cl.plou@gmail.com>
2016-02-06 14:02:24 +0100

    Stable release 2.0.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -22,7 +22,6 @@ module.exports = yeoman.generators.Base.extend({
 
     this.prompt(prompts, function (props) {
       this.props = props;
-      // To access props later use this.props.someOption;
 
       done();
     }.bind(this));

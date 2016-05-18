8c88602477d095ef1c0089e72b8f4c98393231df
Plou <cl.plou@gmail.com>
2016-05-18 20:46:05 +0200

    chores: Remove unecessary semicolons

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,16 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var dummysay = require('dummysay');
+'use strict'
+var yeoman = require('yeoman-generator')
+var chalk = require('chalk')
+var dummysay = require('dummysay')
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
-    var done = this.async();
+    var done = this.async()
 
     // Have the dummy greet the user.
     this.log(dummysay(
       'Welcome to the ' + chalk.blue('dummy') + ' factory!'
-    ));
+    ))
 
     if (this.options['silent']) {
       this.props = {branch: 'master'}
@@ -23,35 +23,35 @@ module.exports = yeoman.generators.Base.extend({
         message: 'Would you like to install the beta version?',
         choices: ['master', 'beta'],
         default: 'master'
-      }];
+      }]
 
       this.prompt(prompts, function (props) {
-        this.props = props;
-        done();
-      }.bind(this));
+        this.props = props
+        done()
+      }.bind(this))
     }
   },
 
   writing: function () {
-    this.copy('readme.md', 'readme.md');
-    var self = this;
+    this.copy('readme.md', 'readme.md')
+    var self = this
     var done = this.async()
     this.remote('dummy-team', 'dummy', this.props.branch, function (err, remote) {
-      self.fs.copy(remote.cachePath + '/index.html', 'index.html');
-      self.fs.copy(remote.cachePath + '/.gitignore', '.gitignore');
-      self.fs.copy(remote.cachePath + '/.editorconfig', '.editorconfig');
-      self.fs.copy(remote.cachePath + '/grunt', './grunt');
-      self.fs.copy(remote.cachePath + '/css', './css');
-      self.fs.copy(remote.cachePath + '/js', './js');
-      self.fs.copy(remote.cachePath + '/img', './img');
-      done();
-    }, true);
+      self.fs.copy(remote.cachePath + '/index.html', 'index.html')
+      self.fs.copy(remote.cachePath + '/.gitignore', '.gitignore')
+      self.fs.copy(remote.cachePath + '/.editorconfig', '.editorconfig')
+      self.fs.copy(remote.cachePath + '/grunt', './grunt')
+      self.fs.copy(remote.cachePath + '/css', './css')
+      self.fs.copy(remote.cachePath + '/js', './js')
+      self.fs.copy(remote.cachePath + '/img', './img')
+      done()
+    }, true)
   },
 
   install: function () {
     if (!this.options['skip-install']) {
-      this.spawnCommandSync('npm', ['install'], { cwd: 'grunt'});
-      this.spawnCommandSync('grunt', ['build'], { cwd: 'grunt'});
+      this.spawnCommandSync('npm', ['install'], { cwd: 'grunt'})
+      this.spawnCommandSync('grunt', ['build'], { cwd: 'grunt'})
     }
   }
-});
+})
